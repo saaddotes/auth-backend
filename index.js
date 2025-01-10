ef2aa03211cb9640476fbce5d0ef949db23021e1
cadetsaad <cadetsaadsoomro@gmail.com>
2025-01-10 22:41:17 +0500

    check

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import users from "./routes/userRoutes.js";
 import dotenv from "dotenv";
-import addTodos from "./routes/todos.js";
+import bcrypt from "bcrypt";
+import User from "../models/userModel.js";
+import checkCredentials from "../middleware/checkCredentials.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 dotenv.config();
 
@@ -19,9 +25,102 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", users);
+const UserSch = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-app.use("/todos", addTodos);
+const User = new mongoose.model("User", UserSch);
+
+function checkCredentials(req, res, next) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email and Password required" });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      success: false,
+      message: "Password must greater then 6 characters",
+    });
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailRegex.test(email)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid Email Format" });
+  }
+
+  next();
+}
+
+app.post("/signup", checkCredentials, async (req, res) => {
+  const { email, password } = req.body;
+  const encryptedPass = await bcrypt.hash(password, 10);
+
+  try {
+    const newUser = await User.create({ email, password: encryptedPass });
+    const safeData = newUser.toObject();
+    delete safeData.password;
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Created User Succesfully",
+      data: safeData,
+      token,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error While adding user : " + e.message,
+    });
+  }
+});
+
+app.post("/login", checkCredentials, async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No User Exists" });
+    }
+
+    const checkPass = await bcrypt.compare(password, user.password);
+
+    if (!checkPass) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect Password" });
+    }
+
+    const safeUser = user.toObject();
+
+    delete safeUser.password;
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "User Founded", data: safeUser, token });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error While adding user" + e.message,
+    });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Server is available at http://localhost:${PORT}`)
